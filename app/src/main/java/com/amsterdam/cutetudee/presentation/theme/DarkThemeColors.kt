8512126999f98649c769ad6d1332e290451b8b9e
Thoraya <thoriahamdy99@gmail.com>
2025-06-15 00:15:59 +0300

    add dark theme colors.

--- app/src/main/java/com/amsterdam/cutetudee/presentation/theme/DarkThemeColors.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/theme/DarkThemeColors.kt
@@ -1,2 +1,34 @@
 package com.amsterdam.cutetudee.presentation.theme
 
+import androidx.compose.ui.graphics.Color
+
+val darkThemeColors = CuteTudeeAppColors(
+    primary = Color(0xFF3090BF),
+    primaryVariant = Color(0xFF05202E),
+    primaryGradientStart = Color(0xFF3090BF),
+    primaryGradientEnd = Color(0xFF3A9CCD),
+    secondary = Color(0xFFCC7851),
+    pinkAccent = Color(0xFFCC5268),
+    yellowAccent = Color(0xFFB28F25),
+    greenAccent = Color(0xFF4D8064),
+    purpleAccent = Color(0xFF6F63B2),
+    error = Color(0xFFF95555),
+    overlay = Color(0x5202151E),
+    emojiTint = Color(0xDE1F1F1F),
+    yellowVariant = Color(0xFF1F1E1C),
+    greenVariant = Color(0xFF1C1F1D),
+    purpleVariant = Color(0xFF1C1A33),
+    errorVariant = Color(0xFF1F1111),
+    title = Color(0xDEFFFFFF),
+    body = Color(0x99FFFFFF),
+    hint = Color(0x61FFFFFF),
+    stroke = Color(0x1FFFFFFF),
+    surfaceLow = Color(0xFF020108),
+    surface = Color(0xFF0D0C14),
+    surfaceHigh = Color(0xFF0F0E19),
+    onPrimary = Color(0xDEFFFFFF),
+    onPrimaryCaption = Color(0xB3FFFFFF),
+    onPrimaryCard = Color(0x29060414),
+    onPrimaryStroke = Color(0x99242424),
+    disable = Color(0xFF1D1E1F)
+)
