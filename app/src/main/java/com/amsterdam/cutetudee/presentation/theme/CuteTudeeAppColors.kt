e68ad6ffc6c740d07ca45e8149eb6a727692eb97
Thoraya <thoriahamdy99@gmail.com>
2025-06-14 23:44:17 +0300

    define cute tudee app colors.

---
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/theme/CuteTudeeAppColors.kt
@@ -0,0 +1,34 @@
+package com.amsterdam.cutetudee.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+data class CuteTudeeAppColors(
+    val primary: Color,
+    val primaryVariant: Color,
+    val primaryGradientStart: Color,
+    val primaryGradientEnd: Color,
+    val secondary: Color,
+    val title: Color,
+    val body: Color,
+    val hint: Color,
+    val stroke: Color,
+    val surfaceLow: Color,
+    val surface: Color,
+    val surfaceHigh: Color,
+    val onPrimary: Color,
+    val onPrimaryCaption: Color,
+    val onPrimaryCard: Color,
+    val onPrimaryStroke: Color,
+    val disable: Color,
+    val pinkAccent: Color,
+    val yellowAccent: Color,
+    val greenAccent: Color,
+    val purpleAccent: Color,
+    val error: Color,
+    val overlay: Color,
+    val emojiTint: Color,
+    val yellowVariant: Color,
+    val greenVariant: Color,
+    val purpleVariant: Color,
+    val errorVariant: Color
+)
