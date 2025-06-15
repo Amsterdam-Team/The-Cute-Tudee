17e4d621b73c872859567309e4d7eaf35cbb8184
khater <khater@email.com>
2025-06-15 12:35:34 +0300

    add required functions for task and category repositories

--- app/src/main/java/com/amsterdam/cutetudee/domain/repository/TaskRepository.kt
+++ app/src/main/java/com/amsterdam/cutetudee/domain/repository/TaskRepository.kt
@@ -1,4 +1,15 @@
 package com.amsterdam.cutetudee.domain.repository
 
+import com.amsterdam.cutetudee.domain.model.Task
+import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.LocalDate
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@OptIn(ExperimentalUuidApi::class)
 interface TaskRepository {
+    suspend fun addTask(task: Task)
+    suspend fun deleteTask(taskId: Uuid)
+    suspend fun getTaskById(taskId: Uuid): Task
+    suspend fun getTaskByDate(date: LocalDate): Flow<List<Task>>
 }
