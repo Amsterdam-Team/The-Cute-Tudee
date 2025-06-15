7717be15b75741bf9bb5ac8aa19f23f74cf36709
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-06-15 13:58:39 +0300

    add the default textStyle for their sizes

---
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/theme/textStyle/DefaultTextStyle.kt
@@ -0,0 +1,102 @@
+package com.amsterdam.cutetudee.presentation.theme.textStyle
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.amsterdam.cutetudee.R
+
+val nunito = FontFamily(
+    Font(R.font.nunito_regular, FontWeight.Normal),
+    Font(R.font.nunito_semi_bold, FontWeight.SemiBold),
+    Font(R.font.nunito_medium, FontWeight.Medium),
+)
+
+val defaultTextStyle = TudeeTextStyle(
+
+    headLine = SizedTextStyle(
+        large = TextStyle(
+            fontFamily = nunito,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 28.sp,
+            lineHeight = 30.sp
+        ),
+        medium = TextStyle(
+            fontFamily = nunito,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 24.sp,
+            lineHeight = 28.sp
+        ),
+        small = TextStyle(
+            fontFamily = nunito,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 20.sp,
+            lineHeight = 24.sp
+        )
+    ),
+
+    title = SizedTextStyle(
+        large = TextStyle(
+            fontFamily = nunito,
+            fontWeight = FontWeight.Medium,
+            fontSize = 18.sp,
+            lineHeight = 22.sp
+        ),
+        medium = TextStyle(
+            fontFamily = nunito,
+            fontWeight = FontWeight.Medium,
+            fontSize = 18.sp,
+            lineHeight = 22.sp
+        ),
+        small = TextStyle(
+            fontFamily = nunito,
+            fontWeight = FontWeight.Medium,
+            fontSize = 16.sp,
+            lineHeight = 20.sp
+        )
+    ),
+
+    body = SizedTextStyle(
+        large = TextStyle(
+            fontFamily = nunito,
+            fontWeight = FontWeight.Normal,
+            fontSize = 18.sp,
+            lineHeight = 22.sp
+        ),
+        medium = TextStyle(
+            fontFamily = nunito,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp,
+            lineHeight = 20.sp
+        ),
+        small = TextStyle(
+            fontFamily = nunito,
+            fontWeight = FontWeight.Normal,
+            fontSize = 14.sp,
+            lineHeight = 17.sp
+        )
+    ),
+
+    label = SizedTextStyle(
+        large = TextStyle(
+            fontFamily = nunito,
+            fontWeight = FontWeight.Medium,
+            fontSize = 16.sp,
+            lineHeight = 19.sp
+        ),
+        medium = TextStyle(
+            fontFamily = nunito,
+            fontWeight = FontWeight.Medium,
+            fontSize = 14.sp,
+            lineHeight = 17.sp
+        ),
+        small = TextStyle(
+            fontFamily = nunito,
+            fontWeight = FontWeight.Medium,
+            fontSize = 12.sp,
+            lineHeight = 16.sp
+        )
+    )
+
+)
