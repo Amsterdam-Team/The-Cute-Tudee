1687e988fb82ae3ba7142197df4a85969b13a04d
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-06-15 13:57:18 +0300

    add data classes for both textStyle and their sizes

---
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/theme/textStyle/TudeeTextStyle.kt
@@ -0,0 +1,19 @@
+package com.amsterdam.cutetudee.presentation.theme.textStyle
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.text.TextStyle
+
+data class TudeeTextStyle(
+    val headLine: SizedTextStyle,
+    val title: SizedTextStyle,
+    val body: SizedTextStyle,
+    val label: SizedTextStyle,
+)
+
+data class SizedTextStyle(
+    val large: TextStyle,
+    val medium: TextStyle,
+    val small: TextStyle
+)
+
+val LocalTudeeTextStyle = staticCompositionLocalOf { defaultTextStyle }
