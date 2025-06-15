49cbf35f13d1308bf496cd74d2ec54277dc86a8c
khater <khater@email.com>
2025-06-15 12:37:45 +0300

    add domain exceptions for tasks and category

--- app/src/main/java/com/amsterdam/cutetudee/domain/exception/Exceptions.kt
+++ app/src/main/java/com/amsterdam/cutetudee/domain/exception/Exceptions.kt
@@ -1,2 +1,31 @@
 package com.amsterdam.cutetudee.domain.exception
 
+import kotlinx.datetime.LocalDate
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+open class CuteTudeeExceptions(message: String) : Exception(message)
+
+
+open class TaskExceptions(message: String) : CuteTudeeExceptions(message)
+
+@OptIn(ExperimentalUuidApi::class)
+class TaskNotFoundException(taskId: Uuid) : TaskExceptions("Task not found with id: $taskId")
+
+class NoTasksFoundPerDateException(date: LocalDate) : TaskExceptions("No tasks found for the given date $date")
+
+class InvalidTaskInputException() : TaskExceptions("Invalid Task Input Exception")
+
+
+open class CategoryExceptions(message: String) : CuteTudeeExceptions(message)
+
+class InvalidCategoryInputException() : CategoryExceptions("Invalid Category Input Exception")
+
+class NoCategoriesFoundException() : CategoryExceptions("No categories found")
+
+@OptIn(ExperimentalUuidApi::class)
+class CategoryNotFoundException(categoryId: Uuid) : TaskExceptions("Category not found with id: $categoryId")
+
+
+
+
