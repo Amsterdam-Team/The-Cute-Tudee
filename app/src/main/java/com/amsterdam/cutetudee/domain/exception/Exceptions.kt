a658c6dd8fd6c782cccb7756495a34517b98ad1b
noor serry <noor.serry.94@gmail.com>
2025-06-15 16:07:10 +0300

    improve custom exceptions naming

--- app/src/main/java/com/amsterdam/cutetudee/domain/exception/Exceptions.kt
+++ app/src/main/java/com/amsterdam/cutetudee/domain/exception/Exceptions.kt
@@ -4,27 +4,27 @@ import kotlinx.datetime.LocalDate
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
-open class CuteTudeeExceptions(message: String) : Exception(message)
+open class CuteTudeeException(message: String) : Exception(message)
 
 
-open class TaskExceptions(message: String) : CuteTudeeExceptions(message)
-
-@OptIn(ExperimentalUuidApi::class)
-class TaskNotFoundException(taskId: Uuid) : TaskExceptions("Task not found with id: $taskId")
-
-class NoTasksFoundPerDateException(date: LocalDate) : TaskExceptions("No tasks found for the given date $date")
-
-class InvalidTaskInputException() : TaskExceptions("Invalid Task Input Exception")
-
-
-open class CategoryExceptions(message: String) : CuteTudeeExceptions(message)
-
-class InvalidCategoryInputException() : CategoryExceptions("Invalid Category Input Exception")
-
-class NoCategoriesFoundException() : CategoryExceptions("No categories found")
+open class TaskException(message: String) : CuteTudeeException(message)
 
 @OptIn(ExperimentalUuidApi::class)
-class CategoryNotFoundException(categoryId: Uuid) : TaskExceptions("Category not found with id: $categoryId")
+class TaskNotFoundException(taskId: Uuid) : TaskException("Task not found with id: $taskId")
+
+class NoTasksFoundPerDateException(date: LocalDate) : TaskException("No tasks found for the given date $date")
+
+class InvalidTaskInputException() : TaskException("Invalid Task Input Exception")
+
+
+open class CategoryException(message: String) : CuteTudeeException(message)
+
+class InvalidCategoryInputException() : CategoryException("Invalid Category Input Exception")
+
+class NoCategoriesFoundException() : CategoryException("No categories found")
+
+@OptIn(ExperimentalUuidApi::class)
+class CategoryNotFoundException(categoryId: Uuid) : CategoryException("Category not found with id: $categoryId")
 
 
 
