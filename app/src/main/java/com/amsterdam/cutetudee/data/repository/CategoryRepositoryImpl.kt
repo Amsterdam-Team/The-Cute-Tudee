ebbd4ce498c35279273c27ae27f8b487053d9359
khater <khater@email.com>
2025-06-15 12:40:02 +0300

    implement repositories methods with TODOs

--- app/src/main/java/com/amsterdam/cutetudee/data/repository/CategoryRepositoryImpl.kt
+++ app/src/main/java/com/amsterdam/cutetudee/data/repository/CategoryRepositoryImpl.kt
@@ -1,6 +1,22 @@
 package com.amsterdam.cutetudee.data.repository
 
+import com.amsterdam.cutetudee.domain.model.Category
 import com.amsterdam.cutetudee.domain.repository.CategoryRepository
+import kotlinx.coroutines.flow.Flow
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
+@OptIn(ExperimentalUuidApi::class)
 class CategoryRepositoryImpl : CategoryRepository {
+    override suspend fun addCategory(category: Category) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteCategory(categoryId: Uuid) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllCategories(): Flow<List<Category>> {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/amsterdam/cutetudee/data/repository/TaskRepositoryImpl.kt
+++ app/src/main/java/com/amsterdam/cutetudee/data/repository/TaskRepositoryImpl.kt
@@ -1,6 +1,27 @@
 package com.amsterdam.cutetudee.data.repository
 
+import com.amsterdam.cutetudee.domain.model.Task
 import com.amsterdam.cutetudee.domain.repository.TaskRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.LocalDate
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
+@OptIn(ExperimentalUuidApi::class)
 class TaskRepositoryImpl : TaskRepository {
+    override suspend fun addTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteTask(taskId: Uuid) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTaskById(taskId: Uuid): Task {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTaskByDate(date: LocalDate): Flow<List<Task>> {
+        TODO("Not yet implemented")
+    }
 }
