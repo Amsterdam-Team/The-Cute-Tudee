a55049633d27a9c7fe34c0e322713c19fc0fd74b
Mona <monaayman584@gmail.com>
2025-06-17 00:17:40 +0300

    add custom navigation button and custom outline button

---
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/component/NavigationButton.kt
@@ -0,0 +1,39 @@
+package com.amsterdam.cutetudee.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.amsterdam.cutetudee.R
+import com.amsterdam.cutetudee.presentation.theme.AppTheme
+import com.amsterdam.cutetudee.presentation.theme.textStyle.defaultTextStyle
+
+@Composable
+fun NavigationButton(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(shape = RoundedCornerShape(100.dp))
+            .background(AppTheme.color.errorVariant)
+            .padding(vertical = 8.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.delete),
+            style = AppTheme.textStyle.label.large,
+            color = AppTheme.color.error
+        )
+    }
+}
---
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/component/OutlineButton.kt
@@ -0,0 +1,41 @@
+package com.amsterdam.cutetudee.presentation.component
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.amsterdam.cutetudee.R
+import com.amsterdam.cutetudee.presentation.theme.AppTheme
+import com.amsterdam.cutetudee.presentation.theme.textStyle.defaultTextStyle
+
+@Composable
+fun OutlineButton(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .border(
+                width = 1.dp,
+                color = AppTheme.color.stroke,
+                shape = RoundedCornerShape(100.dp)
+            )
+            .padding(vertical = 8.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.cancel),
+            style = AppTheme.textStyle.label.large,
+            color = AppTheme.color.primary
+        )
+    }
+}
