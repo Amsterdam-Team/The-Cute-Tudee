d60606c037befdff4f5bd818962eba7a94de8336
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-06-16 19:33:17 +0100

    change CircleShape to RoundCornerShape with 100

--- app/src/main/java/com/amsterdam/cutetudee/presentation/component/tast_status/TaskStatusChip.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/component/tast_status/TaskStatusChip.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,7 +21,7 @@ import com.amsterdam.cutetudee.presentation.theme.AppTheme
 fun TaskStatusChip(
     taskStatusUi: TaskStatusUi,
     modifier: Modifier = Modifier,
-    shape: Shape = CircleShape,
+    shape: Shape = RoundedCornerShape(100),
     contentPadding: PaddingValues = PaddingValues(horizontal = 12.dp, vertical = 6.dp),
 ) {
     Row(
