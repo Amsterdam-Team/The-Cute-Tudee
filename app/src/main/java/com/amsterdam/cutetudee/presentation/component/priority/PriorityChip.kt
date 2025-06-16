ad9c979d34cbd2f0cb6b73dcb80d923bba4faadb
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-06-16 17:14:06 +0100

    add the PriorityUi enum as a parameter instead of its the values

--- app/src/main/java/com/amsterdam/cutetudee/presentation/component/priority/PriorityChip.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/component/priority/PriorityChip.kt
@@ -1,6 +1,5 @@
 package com.amsterdam.cutetudee.presentation.component.priority
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
@@ -12,25 +11,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.amsterdam.cutetudee.presentation.theme.AppTheme
 
 @Composable
 fun PriorityChip(
-    @StringRes label: Int,
-    icon: Painter,
+    priorityUi: PriorityUi,
     selected: Boolean,
-    selectedContainerColor: Color,
     modifier: Modifier = Modifier,
     shape: Shape = CircleShape,
     contentPadding: PaddingValues = PaddingValues(horizontal = 8.dp, vertical = 6.dp)
 ) {
     val contentColor = if (selected) AppTheme.color.onPrimary else AppTheme.color.hint
-    val containerColor = if (selected) selectedContainerColor else AppTheme.color.surfaceLow
+    val containerColor = if (selected) priorityUi.selectedContainerColor else AppTheme.color.surfaceLow
     Row(
         modifier = modifier
             .background(
@@ -42,12 +38,12 @@ fun PriorityChip(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
-            painter = icon,
-            contentDescription = stringResource(label),
+            painter = painterResource(priorityUi.iconRes),
+            contentDescription = stringResource(priorityUi.labelRes),
             tint = contentColor
         )
         Text(
-            text = stringResource(label),
+            text = stringResource(priorityUi.labelRes),
             color = contentColor,
             style = AppTheme.textStyle.label.small
         )
