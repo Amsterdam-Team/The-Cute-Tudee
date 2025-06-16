ed8992cf4912938c7cfd8fe11ddc2517aad294d3
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-06-16 17:09:35 +0100

    add the TaskStatusUi enum as a parameter instead of its the values

--- app/src/main/java/com/amsterdam/cutetudee/presentation/component/tast_status/TaskStatusChip.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/component/tast_status/TaskStatusChip.kt
@@ -1,6 +1,5 @@
 package com.amsterdam.cutetudee.presentation.component.tast_status
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -20,9 +18,7 @@ import com.amsterdam.cutetudee.presentation.theme.AppTheme
 
 @Composable
 fun TaskStatusChip(
-    @StringRes labelRes: Int,
-    containerColor: Color,
-    contentColor: Color,
+    taskStatusUi: TaskStatusUi,
     modifier: Modifier = Modifier,
     shape: Shape = CircleShape,
     contentPadding: PaddingValues = PaddingValues(horizontal = 12.dp, vertical = 6.dp),
@@ -30,7 +26,7 @@ fun TaskStatusChip(
     Row(
         modifier = modifier
             .background(
-                color = containerColor,
+                color = taskStatusUi.containerColor,
                 shape = shape
             )
             .padding(contentPadding),
@@ -40,14 +36,14 @@ fun TaskStatusChip(
         Box(
             modifier = Modifier
                 .background(
-                    color = contentColor,
+                    color = taskStatusUi.contentColor,
                     shape = shape
                 )
                 .padding(2.5.dp)
         )
         Text(
-            text = stringResource(labelRes),
-            color = contentColor,
+            text = stringResource(taskStatusUi.labelRes),
+            color = taskStatusUi.contentColor,
             style = AppTheme.textStyle.label.small
         )
     }
