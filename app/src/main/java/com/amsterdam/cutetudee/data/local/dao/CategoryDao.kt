979985f2edd20a0d0c6b4216436aa337d1d6076b
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-06-16 19:36:32 +0300

    add task and category DAOs with the required functionalities add, edit, get, delete

--- app/src/main/java/com/amsterdam/cutetudee/data/local/dao/CategoryDao.kt
+++ app/src/main/java/com/amsterdam/cutetudee/data/local/dao/CategoryDao.kt
@@ -1,4 +1,23 @@
 package com.amsterdam.cutetudee.data.local.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.amsterdam.cutetudee.data.local.entity.CategoryEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface CategoryDao {
+    @Insert
+    suspend fun addCategory(category: CategoryEntity)
+
+    @Update
+    suspend fun editCategory(category: CategoryEntity)
+
+    @Query("DELETE FROM Category WHERE id = :id")
+    suspend fun deleteCategory(id: String)
+
+    @Query("SELECT * FROM Category")
+    fun getAllCategories(): Flow<List<CategoryEntity>>
 }
--- app/src/main/java/com/amsterdam/cutetudee/data/local/dao/TaskDao.kt
+++ app/src/main/java/com/amsterdam/cutetudee/data/local/dao/TaskDao.kt
@@ -1,5 +1,29 @@
 package com.amsterdam.cutetudee.data.local.dao
 
-interface TaskDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.amsterdam.cutetudee.data.local.entity.TaskEntity
+import kotlinx.coroutines.flow.Flow
 
+@Dao
+interface TaskDao {
+    @Insert
+    suspend fun addTask(task: TaskEntity)
+
+    @Update
+    suspend fun editTask(task: TaskEntity)
+
+    @Query("DELETE FROM Task WHERE id = :id")
+    suspend fun deleteTask(id: String)
+
+    @Query("SELECT * FROM Task WHERE id = :id")
+    suspend fun getTaskById(id: String): TaskEntity
+
+    @Query("SELECT * FROM Task WHERE targetDate = :date")
+    fun getTaskByDate(date: Long): Flow<List<TaskEntity>>
+
+    @Query("SELECT * FROM Task WHERE categoryId = :categoryId")
+    fun getTasksByCategoryId(categoryId: String): Flow<List<TaskEntity>>
 }
