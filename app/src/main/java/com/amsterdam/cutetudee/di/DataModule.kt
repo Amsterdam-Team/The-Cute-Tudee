72e67a536ee33ba3fdb453053d5bce321b41f3ac
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-06-16 19:55:31 +0300

    add TudeeDatabase, DAOs and repositories to koin

--- app/src/main/java/com/amsterdam/cutetudee/di/DataModule.kt
+++ app/src/main/java/com/amsterdam/cutetudee/di/DataModule.kt
@@ -1,7 +1,29 @@
 package com.amsterdam.cutetudee.di
 
+import com.amsterdam.cutetudee.data.local.roomDB.TudeeDatabase
+import com.amsterdam.cutetudee.data.repository.CategoryRepositoryImpl
+import com.amsterdam.cutetudee.data.repository.TaskRepositoryImpl
+import com.amsterdam.cutetudee.domain.repository.CategoryRepository
+import com.amsterdam.cutetudee.domain.repository.TaskRepository
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 val dataModule = module {
 
+    single {
+        TudeeDatabase.getInstance(androidApplication())
+    }
+
+    single {
+        get<TudeeDatabase>().categoryDao()
+        get<TudeeDatabase>().taskDao()
+    }
+
+    single<CategoryRepository> {
+        CategoryRepositoryImpl(get())
+    }
+
+    single<TaskRepository> {
+        TaskRepositoryImpl(get())
+    }
 }
