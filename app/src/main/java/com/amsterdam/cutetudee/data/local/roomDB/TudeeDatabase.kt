d7b6960fcf9de025f1d8de8c1512c8a8ef1fc0d6
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-06-16 19:38:47 +0300

    rename LocalDatabase to TudeeDatabase and implement RoomDatabase

---
+++ app/src/main/java/com/amsterdam/cutetudee/data/local/roomDB/TudeeDatabase.kt
@@ -0,0 +1,35 @@
+package com.amsterdam.cutetudee.data.local.roomDB
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.amsterdam.cutetudee.data.local.dao.CategoryDao
+import com.amsterdam.cutetudee.data.local.dao.TaskDao
+import com.amsterdam.cutetudee.data.local.entity.CategoryEntity
+import com.amsterdam.cutetudee.data.local.entity.TaskEntity
+
+@Database(entities = [CategoryEntity::class, TaskEntity::class], version = 1)
+abstract class TudeeDatabase : RoomDatabase() {
+    abstract fun categoryDao(): CategoryDao
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        private const val DATABASE_NAME = "TudeeDatabase"
+
+        @Volatile
+        private var instance: TudeeDatabase? = null
+
+        fun getInstance(context: Context): TudeeDatabase {
+            return instance ?: synchronized(this) {
+                buildDatabase(context).also {
+                    instance = it
+                }
+            }
+        }
+
+        fun buildDatabase(context: Context): TudeeDatabase {
+            return Room.databaseBuilder(context, TudeeDatabase::class.java, DATABASE_NAME).build()
+        }
+    }
+}
