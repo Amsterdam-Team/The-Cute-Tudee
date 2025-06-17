8df160088c49f240f2c49e0acb5306bb12cc0495
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-06-17 12:37:27 +0100

    remove ThemeTypes file & move the type to AppTheme

--- app/src/main/java/com/amsterdam/cutetudee/presentation/theme/ThemeTypes.kt
+++
@@ -1,6 +0,0 @@
-package com.amsterdam.cutetudee.presentation.theme
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-
-typealias ColorType = @Composable () -> Color
