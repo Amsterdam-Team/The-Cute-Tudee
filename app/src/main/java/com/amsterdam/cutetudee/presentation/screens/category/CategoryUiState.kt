bbb237e52278a03b647aad622bd49466db8ae211
Mona <monaayman584@gmail.com>
2025-06-17 10:21:42 +0300

    add default values at parameter in CategoryUiState

--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/category/CategoryUiState.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/category/CategoryUiState.kt
@@ -1,10 +1,11 @@
 package com.amsterdam.cutetudee.presentation.screens.category
 
+
 import androidx.compose.ui.graphics.painter.Painter
 
 
 data class CategoryItemUiState(
-    val categoryImage: Painter,
-    val  categoryName: String,
-    val badgeText: String
+    val categoryImage: Painter ,
+    val categoryName: String = "",
+    val badgeText: String = ""
 )
