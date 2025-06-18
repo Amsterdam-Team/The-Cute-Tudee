3efd9fa67aa4a951b53e8a04d4af2741c28cf1d7
khaatteerr <hamdikhater68@gmail.com>
2025-06-19 00:43:48 +0300

    implement `DeleteConfirmationBottomSheet` with Material 3.

--- app/src/main/java/com/amsterdam/cutetudee/presentation/component/DeleteConfirmationBottomSheet.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/component/DeleteConfirmationBottomSheet.kt
@@ -3,41 +3,92 @@ package com.amsterdam.cutetudee.presentation.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.amsterdam.cutetudee.R
 import com.amsterdam.cutetudee.presentation.theme.AppTheme
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DeleteConfirmationBottomSheet(
     modifier: Modifier = Modifier,
+    isVisible: Boolean,
+    onDelete: () -> Unit,
+    onCancel: () -> Unit,
+    onDismiss: () -> Unit
 ) {
-    CustomBottomSheet(modifier) {
-        ConfirmationMessageContainer()
-        ConfirmationButtonContainer()
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true,
+        confirmValueChange = { true }
+    )
+    val coroutineScope = rememberCoroutineScope()
+    val dismissBottomSheet = remember {
+        {
+            coroutineScope.launch {
+                sheetState.hide()
+            }.invokeOnCompletion {
+                if (!sheetState.isVisible) {
+                    onDismiss()
+                }
+            }
+        }
+    }
+
+    if (isVisible) {
+        CustomBottomSheet(
+            modifier = modifier,
+            onDismissRequest = { dismissBottomSheet() },
+            sheetState = sheetState
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ConfirmationMessageContainer()
+                ConfirmationButtonContainer(
+                    onDelete = {
+                        onDelete()
+                        dismissBottomSheet()
+                    },
+                    onCancel = {
+                        onCancel()
+                        dismissBottomSheet()
+                    }
+                )
+            }
+        }
     }
 }
 
 @Composable
 private fun ConfirmationButtonContainer(
     modifier: Modifier = Modifier,
+    onDelete: () -> Unit,
+    onCancel: () -> Unit
 ) {
     Column(
         modifier = modifier
             .fillMaxWidth()
+            .wrapContentHeight()
             .shadow(
                 elevation = 4.dp,
                 shape = RectangleShape,
@@ -58,14 +109,14 @@ private fun ConfirmationButtonContainer(
     ) {
         GradientFilledButton(
             title = stringResource(R.string.delete),
-            onClick = { },
+            onClick = onDelete,
             isLoading = false,
             isNegative = true,
             modifier = Modifier.fillMaxWidth(),
         )
         OutlineButton(
             text = stringResource(R.string.cancel),
-            onClick = {},
+            onClick = onCancel,
             isLoading = false,
             modifier = Modifier.fillMaxWidth(),
         )
@@ -106,8 +157,3 @@ private fun ConfirmationMessageContainer(
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-private fun DeleteConfirmationBottomSheetPreview() {
-    DeleteConfirmationBottomSheet()
-}
