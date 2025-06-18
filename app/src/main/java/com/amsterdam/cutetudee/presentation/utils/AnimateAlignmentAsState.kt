4e56f2e7217c3a772828100fab616377d017f124
Eslam <eslam.mohamed.551999@gmail.com>
2025-06-19 01:13:40 +0300

    add animated theme switcher button

---
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/utils/AnimateAlignmentAsState.kt
@@ -0,0 +1,28 @@
+package com.amsterdam.cutetudee.presentation.utils
+
+import android.annotation.SuppressLint
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun animateAlignmentAsState(
+    targetAlignment: Alignment,
+): State<Alignment> {
+    val biased = targetAlignment as BiasAlignment
+    val horizontal by animateFloatAsState(
+        biased.horizontalBias,
+        animationSpec = tween(durationMillis = 1000)
+    )
+    val vertical by animateFloatAsState(
+        biased.verticalBias,
+        animationSpec = tween(durationMillis = 1000)
+    )
+    return derivedStateOf { BiasAlignment(horizontal, vertical) }
+}
