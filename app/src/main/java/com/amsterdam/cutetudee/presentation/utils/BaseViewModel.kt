07335c0ee5aeb0bd9c07f2324cc0bc5e1c1c5619
noor serry <noor.serry.94@gmail.com>
2025-06-18 23:44:03 +0300

    refactor: Move exception mapping to separate file

--- app/src/main/java/com/amsterdam/cutetudee/presentation/utils/BaseViewModel.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/utils/BaseViewModel.kt
@@ -2,8 +2,6 @@ package com.amsterdam.cutetudee.presentation.utils
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.amsterdam.cutetudee.R
-import com.amsterdam.cutetudee.domain.exception.*
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -26,22 +24,12 @@ class BaseViewModel <T>(initialState: T): ViewModel() {
                 val result = function()
                 onSuccess(result)
             } catch (exception : Exception){
-                val errorResourceId = mapExceptionToResourceId(exception)
+                val errorResourceId = exception.mapExceptionToResourceId()
                 onError(errorResourceId)
             }
         }
     }
 
-    private fun mapExceptionToResourceId(exception: Exception): Int {
-        return when (exception) {
-            is TaskNotFoundException -> R.string.error_task_not_found
-            is NoTasksFoundPerDateException -> R.string.error_no_tasks_found_per_date
-            is InvalidTaskInputException -> R.string.error_invalid_task_input
-            is InvalidCategoryInputException -> R.string.error_invalid_category_input
-            is NoCategoriesFoundException -> R.string.error_no_categories_found
-            is CategoryNotFoundException -> R.string.error_category_not_found
-            else -> R.string.error_unknown
-        }
-    }
+
 
 }
