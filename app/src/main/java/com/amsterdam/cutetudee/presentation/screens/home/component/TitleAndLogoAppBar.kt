8dc8a29e8f4b490a563a4d7418ed3d891204c2cb
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-06-19 19:14:48 +0300

    add TitleAndLogoAppBar composable for displaying title and logo in the home screen

---
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/home/component/TitleAndLogoAppBar.kt
@@ -0,0 +1,67 @@
+package com.amsterdam.cutetudee.presentation.screens.home.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.amsterdam.cutetudee.R
+import com.amsterdam.cutetudee.presentation.theme.AppTheme
+
+@Composable
+fun TitleAndLogoAppBar(
+    title: String,
+    description: String,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .height(48.dp)
+                .background(
+                    AppTheme.color.surfaceHigh.copy(alpha = 0.4f),
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .border(
+                    1.dp,
+                    AppTheme.color.surfaceHigh.copy(alpha = 0.4f),
+                    shape = RoundedCornerShape(12.dp)
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(R.drawable.header_image),
+                contentDescription = null,
+                modifier = Modifier
+            )
+        }
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = title,
+                color = AppTheme.color.onPrimary,
+                style = AppTheme.textStyle.title.large
+            )
+            Text(
+                text = description,
+                color = AppTheme.color.onPrimaryCaption,
+                style = AppTheme.textStyle.label.small
+            )
+        }
+    }
+}
