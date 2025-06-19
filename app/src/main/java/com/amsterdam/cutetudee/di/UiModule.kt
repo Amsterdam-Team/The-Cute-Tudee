b390deee914fc7b225480baecd75145dff7e819d
marah222 <maraharam222@gmail.com>
2025-06-19 18:44:32 +0300

    add categoryDetails screen to  UiModel

--- app/src/main/java/com/amsterdam/cutetudee/di/UiModule.kt
+++ app/src/main/java/com/amsterdam/cutetudee/di/UiModule.kt
@@ -1,6 +1,9 @@
 package com.amsterdam.cutetudee.di
 
+import com.amsterdam.cutetudee.presentation.screens.categoryDetails.CategoryDetailsViewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val uiModule = module {
+    viewModelOf(::CategoryDetailsViewModel)
 }
