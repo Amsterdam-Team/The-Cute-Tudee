e54c24cbd4effa6ac8c91e74abaefb1eae70ecb9
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-06-19 19:14:19 +0300

    add category screen ui state data class

--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/category/CategoryUiState.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/category/CategoryUiState.kt
@@ -1,11 +1,22 @@
 package com.amsterdam.cutetudee.presentation.screens.category
 
-
 import androidx.compose.ui.graphics.painter.Painter
 
+data class CategoryScreenUiState(
+    val categories: List<CategoryUiState> = emptyList(),
+    val isLoading: Boolean = false,
+    val errorMessageResourceId: Int? = null
+)
 
 data class CategoryItemUiState(
     val categoryImage: Painter ,
+    val categoryName: String = "",
+    val badgeCount: String = ""
+)
+
+data class CategoryUiState(
+    val categoryId: String,
+    val categoryImage: String ,
     val categoryName: String = "",
     val badgeCount: String = ""
 )
