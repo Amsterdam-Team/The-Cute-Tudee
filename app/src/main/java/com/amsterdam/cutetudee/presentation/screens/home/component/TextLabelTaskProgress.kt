a6dd3cd619054e5067f12153011c8d8d682d04e3
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-06-19 19:15:50 +0300

    add TextLabelTaskProgress composable for displaying task label and item count

---
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/home/component/TextLabelTaskProgress.kt
@@ -0,0 +1,28 @@
+package com.amsterdam.cutetudee.presentation.screens.home.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.amsterdam.cutetudee.presentation.theme.AppTheme
+
+@Composable
+fun TextLabelTaskProgress(
+    label: String, numbersOfItems: Int, modifier: Modifier = Modifier, onClick: () -> Unit
+) {
+    Row(
+        modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = label, style = AppTheme.textStyle.title.large, color = AppTheme.color.title
+        )
+        TextWithArrowIcon(
+            numbersOfItems = numbersOfItems
+        ) { onClick() }
+    }
+}
