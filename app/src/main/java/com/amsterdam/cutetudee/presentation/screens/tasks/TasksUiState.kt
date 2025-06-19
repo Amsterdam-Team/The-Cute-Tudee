18ba9d27143a514fa8ba8fc0b9361d3881b30d96
Mona <monaayman584@gmail.com>
2025-06-19 16:46:15 +0300

    add parameter for TaskUiState

--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/tasks/TasksUiState.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/tasks/TasksUiState.kt
@@ -1,2 +1,18 @@
 package com.amsterdam.cutetudee.presentation.screens.tasks
 
+import com.amsterdam.cutetudee.domain.model.Task
+import com.amsterdam.cutetudee.presentation.component.chip.tast_status.TaskStatusUi
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+
+
+data class TasksUiState(
+    val currentDate: LocalDate = Clock.System.now()
+        .toLocalDateTime(TimeZone.currentSystemDefault()).date,
+    val notificationBadge: Int = 0,
+    val tasks: List<Task> = emptyList(),
+    val currentSelectedTaskStatusUi: TaskStatusUi= TaskStatusUi.TODO
+
+)
