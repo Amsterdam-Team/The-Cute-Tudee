4eae3f54ecc28d8f6535ff996fe012c54dd4051d
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-06-19 19:15:03 +0300

    add TextWithArrowIcon composable for displaying item count with an arrow icon

---
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/home/component/TextWithArrowIcon.kt
@@ -0,0 +1,57 @@
+package com.amsterdam.cutetudee.presentation.screens.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.amsterdam.cutetudee.R
+import com.amsterdam.cutetudee.presentation.theme.AppTheme
+
+@Composable
+fun TextWithArrowIcon(
+    numbersOfItems: Int,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    Row(
+        modifier
+            .background(
+                AppTheme.color.surfaceHigh,
+                shape = RoundedCornerShape(100.dp)
+            )
+            .padding(horizontal = 8.dp, vertical = 6.dp)
+            .clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        Text(
+            text = "$numbersOfItems",
+            style = AppTheme.textStyle.label.medium,
+            color = AppTheme.color.body
+        )
+        Icon(
+            painter = painterResource(R.drawable.arrow),
+            contentDescription = null,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TextWithArrowIcon() {
+    TextWithArrowIcon(
+        numbersOfItems = 12,
+        onClick = {},
+    )
+
+}
