9b18fd183adb1e0eebfd6048ac5ceaac6e57309c
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-06-19 02:15:32 +0100

    refactor the TaskItemCard with a swipe to reveal behaviour with actions

--- app/src/main/java/com/amsterdam/cutetudee/presentation/component/task_card/TaskItemCard.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/component/task_card/TaskItemCard.kt
@@ -1,57 +1,146 @@
 package com.amsterdam.cutetudee.presentation.component.task_card
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.amsterdam.cutetudee.R
 import com.amsterdam.cutetudee.presentation.component.chip.DateChip
 import com.amsterdam.cutetudee.presentation.component.chip.priority.PriorityChip
 import com.amsterdam.cutetudee.presentation.component.chip.priority.PriorityUi
 import com.amsterdam.cutetudee.presentation.theme.AppTheme
+import com.amsterdam.cutetudee.presentation.theme.CuteTudeeTheme
+import com.amsterdam.cutetudee.presentation.utils.ThemeAndLocalePreviews
+import kotlin.math.roundToInt
 
 @Composable
 fun TaskItemCard(
     taskItemUiState: TaskItemUiState,
     modifier: Modifier = Modifier,
+    shape: Shape = RoundedCornerShape(16.dp),
+    isDeletable: Boolean = false,
+    onDeleteAction: () -> Unit = {},
+    onClick: () -> Unit = {},
 ) {
-    Column(
-        modifier = modifier
-            .background(
-                color = AppTheme.color.surfaceHigh,
-                shape = RoundedCornerShape(16.dp)
-            )
-            .padding(
-                start = 4.dp, top = 4.dp, end = 12.dp, bottom = 12.dp
-            ),
-        verticalArrangement = Arrangement.spacedBy(2.dp)
-    ) {
-        TaskItemHeader(
-            taskItemUiState = taskItemUiState,
-            showDate = taskItemUiState.date.isNotEmpty()
-        )
-        TaskItemInfo(
-            taskItemUiState = taskItemUiState,
-            showDescription = taskItemUiState.description.isNotEmpty()
-        )
 
+    val maxOffsetPx = with(LocalDensity.current) { -56.dp.toPx() }
+    val defaultOffset = 0f
+    var draggedOffsetX by remember { mutableFloatStateOf(0f) }
+    val state = rememberDraggableState { delta ->
+        draggedOffsetX = (draggedOffsetX + delta * 1.75f).coerceIn(maxOffsetPx, defaultOffset)
+    }
+    val animatedOffsetX by animateFloatAsState(
+        targetValue = if (isDeletable) draggedOffsetX else defaultOffset,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioLowBouncy,
+        ),
+    )
+
+    Box {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .align(Alignment.Center)
+                .background(
+                    color = AppTheme.color.errorVariant, shape = shape
+                )
+                .padding(horizontal = 12.dp, vertical = 40.dp),
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.delete_icon),
+                tint = AppTheme.color.error,
+                contentDescription = "delete icon",
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+                    .size(32.dp)
+                    .clip(shape)
+                    .clickable(
+                        onClick = onDeleteAction, role = Role.Button
+                    )
+            )
+        }
+
+        Column(
+            modifier = modifier
+                .offset { IntOffset(animatedOffsetX.roundToInt(), 0) }
+                .draggable(
+                    orientation = Orientation.Horizontal,
+                    state = state,
+                    onDragStarted = {
+                        draggedOffsetX = maxOffsetPx
+                    },
+                    onDragStopped = {
+                        draggedOffsetX =
+                            if (draggedOffsetX < maxOffsetPx / 2) maxOffsetPx else defaultOffset
+                    },
+                    reverseDirection = LocalLayoutDirection.current == LayoutDirection.Rtl
+                )
+                .background(
+                    color = AppTheme.color.surfaceHigh, shape = shape
+                )
+                .clip(shape)
+                .clickable(
+                    onClick = {
+                        draggedOffsetX = defaultOffset
+                        onClick()
+                    }, role = Role.Button
+                )
+                .padding(
+                    start = 4.dp, top = 4.dp, end = 12.dp, bottom = 12.dp
+                ),
+            verticalArrangement = Arrangement.spacedBy(2.dp)
+        ) {
+            TaskItemHeader(
+                taskItemUiState = taskItemUiState, showDate = taskItemUiState.date.isNotEmpty()
+            )
+            TaskItemInfo(
+                taskItemUiState = taskItemUiState,
+                showDescription = taskItemUiState.description.isNotEmpty()
+            )
+        }
     }
 }
+
 
 @Composable
 private fun TaskItemHeader(
@@ -61,8 +150,7 @@ private fun TaskItemHeader(
 ) {
 
     Row(
-        modifier = modifier
-            .fillMaxWidth(),
+        modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(4.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -78,17 +166,14 @@ private fun TaskItemHeader(
             DateChip(taskItemUiState.date)
         }
         PriorityChip(
-            priorityUi = taskItemUiState.priorityUi,
-            isSelected = true
+            priorityUi = taskItemUiState.priorityUi, isSelected = true
         )
     }
 }
 
 @Composable
 private fun TaskItemInfo(
-    taskItemUiState: TaskItemUiState,
-    showDescription: Boolean,
-    modifier: Modifier = Modifier
+    taskItemUiState: TaskItemUiState, showDescription: Boolean, modifier: Modifier = Modifier
 ) {
     Column(
         modifier = modifier.padding(start = 8.dp),
@@ -111,17 +196,31 @@ private fun TaskItemInfo(
     }
 }
 
-@Preview(showBackground = true)
+@ThemeAndLocalePreviews
 @Composable
 private fun TaskCardPreview() {
-    TaskItemCard(
-        taskItemUiState = TaskItemUiState(
-            categoryImage = painterResource(R.drawable.developer_icon),
-            priorityUi = PriorityUi.MEDIUM,
-            name = "Organize Study Desk",
-            description = "Review cell structure and functions for tomorrow, Review cell structure and functions for tomorrow",
-            date = "12-03-2025",
-        ),
-        modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp)
-    )
+    var deleted by remember { mutableStateOf(false) }
+    CuteTudeeTheme(isSystemInDarkTheme()) {
+        TaskItemCard(
+            taskItemUiState = TaskItemUiState(
+                categoryImage = painterResource(R.drawable.book_open_icon),
+                priorityUi = PriorityUi.MEDIUM,
+                name = stringResource(R.string.empty_screen_title),
+                description = stringResource(R.string.empty_screen_description),
+                date = "12-03-2025",
+            ),
+            isDeletable = false,
+            onDeleteAction = {
+                deleted = !deleted
+            },
+            onClick = {
+                deleted = !deleted
+            }, modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp)
+        )
+
+        AnimatedVisibility(deleted) {
+            Box(modifier = Modifier.fillMaxSize())
+        }
+
+    }
 }
