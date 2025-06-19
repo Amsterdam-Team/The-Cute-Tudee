41bece0a50fec1582328587f54bb4da8a43ff336
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-06-19 18:09:59 +0100

    update the schema of the database to one to many relationship

--- app/src/main/java/com/amsterdam/cutetudee/data/local/entity/TaskEntity.kt
+++ app/src/main/java/com/amsterdam/cutetudee/data/local/entity/TaskEntity.kt
@@ -1,9 +1,20 @@
 package com.amsterdam.cutetudee.data.local.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Task")
+@Entity(
+    tableName = "Task",
+    foreignKeys = [
+        ForeignKey(
+            entity = CategoryEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["categoryId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class TaskEntity(
     @PrimaryKey val id: String,
     val title: String,
