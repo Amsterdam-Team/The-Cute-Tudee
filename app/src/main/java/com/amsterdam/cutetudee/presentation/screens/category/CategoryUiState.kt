b94c2ae47c8c51f89bf8e68c8fb19f088598a77a
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-06-19 12:36:51 +0300

    Add Category viewmodel

--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/category/CategoryUiState.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/category/CategoryUiState.kt
@@ -1,11 +1,24 @@
 package com.amsterdam.cutetudee.presentation.screens.category
 
 
+import android.net.Uri
 import androidx.compose.ui.graphics.painter.Painter
 
+data class CategoryUiState(
+    val categories: List<CategoryItemUiState> = emptyList(),
+    val addBottomSheet: BottomSheetState = BottomSheetState(),
+    val showBottomSheet: Boolean = false
+)
 
 data class CategoryItemUiState(
     val categoryImage: Painter ,
     val categoryName: String = "",
     val badgeCount: String = ""
+)
+
+data class BottomSheetState(
+    val name: String = "",
+    val image: Uri = Uri.EMPTY,
+    val isEnabled: Boolean = false,
+    val isLoading: Boolean = false
 )
--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/category/CategoryViewModel.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/category/CategoryViewModel.kt
@@ -1,8 +1,115 @@
 package com.amsterdam.cutetudee.presentation.screens.category
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.amsterdam.cutetudee.domain.model.Category
+import com.amsterdam.cutetudee.domain.repository.CategoryService
+import com.amsterdam.cutetudee.presentation.utils.UriToBitmapString
+import com.amsterdam.cutetudee.presentation.utils.ValidateImageSize
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class CategoryViewModel : ViewModel() {
+@OptIn(kotlin.uuid.ExperimentalUuidApi::class)
+class CategoryViewModel(
+    private val service: CategoryService,
+    private val validateImageSize: ValidateImageSize,
+    private val uriToBitmapString: UriToBitmapString
+) : ViewModel() {
 
+    private val _state = MutableStateFlow(CategoryUiState())
+    val state = _state.asStateFlow()
 
+    // region Add Category
+
+    fun updateCategoryName(name: String){
+        _state.update {
+            it.copy(
+                addBottomSheet = it.addBottomSheet.copy(
+                    name = name,
+                )
+            )
+        }
+        shouldEnableLoading(name = state.value.addBottomSheet.name, uri = state.value.addBottomSheet.image)
+    }
+
+    fun updateCategoryImage(uri: Uri){
+        _state.update {
+            it.copy(
+                addBottomSheet = it.addBottomSheet.copy(
+                    image = uri,
+
+                )
+            )
+        }
+        shouldEnableLoading(name = state.value.addBottomSheet.name, uri = state.value.addBottomSheet.image)
+    }
+
+    private fun shouldEnableLoading(name: String, uri: Uri){
+        if (name != "" && uri != Uri.EMPTY){
+            _state.update {
+                it.copy(
+                    addBottomSheet = it.addBottomSheet.copy(
+                        isEnabled = true
+                    )
+                )
+            }
+        } else {
+            _state.update {
+                it.copy(
+                    addBottomSheet = it.addBottomSheet.copy(
+                        isEnabled = false
+                    )
+                )
+            }
+        }
+    }
+
+    fun dismissBottomSheet(){
+        _state.update {
+            it.copy(
+                addBottomSheet = it.addBottomSheet.copy(
+                    name = "",
+                    image = Uri.EMPTY
+                ),
+                showBottomSheet = false
+            )
+        }
+    }
+
+    fun addCategory(){
+        if (!validateImageSize(state.value.addBottomSheet.image)){
+            return
+        }
+
+        viewModelScope.launch {
+            _state.update {
+                it.copy(
+                    addBottomSheet = it.addBottomSheet.copy(
+                        isLoading = true
+                    )
+                )
+            }
+            service.addCategory(
+                Category(
+                    imageUrl = uriToBitmapString.uriToBase64(state.value.addBottomSheet.image),
+                    name = state.value.addBottomSheet.name,
+                    numberOfTasks = 0
+                )
+            )
+            delay(5000)
+            _state.update {
+                it.copy(
+                    addBottomSheet = it.addBottomSheet.copy(
+                        isLoading = false
+                    )
+                )
+            }
+        }
+
+    }
+    // endregion
 }
