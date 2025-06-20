51b1878946f675325acd3f5772f00753d5ea58ca
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-06-20 04:42:09 +0300

    feat add functions to get month days

--- app/src/main/java/com/amsterdam/cutetudee/presentation/utils/DateTimeUtils.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/utils/DateTimeUtils.kt
@@ -1,18 +1,23 @@
 package com.amsterdam.cutetudee.presentation.utils
 
-import androidx.compose.ui.text.intl.Locale
-import java.time.YearMonth
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.DayOfWeek
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.plus
+import kotlinx.datetime.until
 import java.time.format.TextStyle
+import java.util.Locale
 
-fun getNumberOfDays(): List<Int> {
-    val currentMonth = YearMonth.now()
-
-    return (1..currentMonth.lengthOfMonth()).map { day ->
-        day
-    }
-
+fun LocalDate.monthDays(): List<Int> {
+    val start = this
+    val end = start.plus(1, DateTimeUnit.MONTH)
+    val daysInMonth = start.until(end, DateTimeUnit.DAY)
+    return (1..daysInMonth).toList()
 }
 
-fun getCurrentMonthDays(day: Int):  String {
-   return YearMonth.now().atDay(day).dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.current.platformLocale)
+fun LocalDate.getCurrentMonthDays(day: Int): String {
+    val now = this
+    val date = LocalDate(now.year, now.month, day)
+    val dayOfWeek = DayOfWeek.valueOf(date.dayOfWeek.name)
+    return dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
 }
