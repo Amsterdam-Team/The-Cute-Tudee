fc60e31101aac47b803a5224254930a9d45fb406
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-06-20 09:44:27 +0300

    refactor HomeUiState to include moodState and enhance MoodState enum with resource references

--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/home/HomeUiState.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/home/HomeUiState.kt
@@ -1,21 +1,23 @@
 package com.amsterdam.cutetudee.presentation.screens.home
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.amsterdam.cutetudee.R
 import com.amsterdam.cutetudee.domain.model.Task
 import com.amsterdam.cutetudee.presentation.component.chip.priority.PriorityUi
 
 data class HomeUiState(
-    val currentDate : String = "",
+    val currentDate: String = "",
     val inProgressTasks: List<TaskDetails> = emptyList(),
     val todoTasks: List<TaskDetails> = emptyList(),
     val doneTasks: List<TaskDetails> = emptyList(),
-    val toDoTasksNumber : Int = 0,
-    val inProgressTasksNumber : Int = 0,
-    val doneTasksNumber : Int = 0,
-    val totalTasksNumber : Int = 0,
-    val isLoading :Boolean = false,
-   @StringRes val errorMessageId : Int? = null
+    val toDoTasksNumber: Int = 0,
+    val inProgressTasksNumber: Int = 0,
+    val doneTasksNumber: Int = 0,
+    val totalTasksNumber: Int = 0,
+    val moodState: MoodState = MoodState.STAY_WORKING,
+    val isLoading: Boolean = false,
+    @StringRes val errorMessageId: Int? = null
 ) {
     data class TaskDetails(
         val icon: String = "",
@@ -26,21 +28,34 @@ data class HomeUiState(
     )
 }
 
-
-//data class TaskBottomSheetState(
-//    val taskId: Long = -1,
-//    val title: String = "",
-//    val description: String = "",
-//    val date: String = "",
-//    val priority: Task.Priority = Task.Priority.LOW,
-//    val categoryId: Long = -1L,
-//    val isEditMode: Boolean = false
-//)
-//
-//
-//enum class MoodState {
-//    STAY_WORKING,
-//    TADOO,
-//    ZERO_PROGRESS,
-//    NOTHING_IN_YOUR_LIST
-//}
+enum class MoodState(
+    @StringRes val description: Int,
+    @StringRes val title: Int,
+    @DrawableRes val icon: Int,
+    @DrawableRes val pinter: Int
+) {
+    STAY_WORKING(
+        description = R.string.update_neutral_description,
+        title = R.string.update_neutral_title,
+        icon = R.drawable.okay_emoji_icon,
+        pinter = R.drawable.tudee_image_neutral
+    ),
+    TADOO(
+        description = R.string.update_happy_description,
+        title = R.string.update_happy_title,
+        icon = R.drawable.good_emoji_icon,
+        pinter = R.drawable.tudee_image_happy
+    ),
+    ZERO_PROGRESS(
+        description = R.string.update_angry_description,
+        title = R.string.update_angry_title,
+        icon = R.drawable.bad_emoji_icon,
+        pinter = R.drawable.tudee_image_sad
+    ),
+    NOTHING_IN_YOUR_LIST(
+        description = R.string.update_sad_description,
+        title = R.string.update_sad_title,
+        icon = R.drawable.poor_emoji_icon,
+        pinter = R.drawable.tudee_image_sad
+    )
+}
