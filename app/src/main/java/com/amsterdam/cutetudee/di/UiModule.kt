add59812d38e98705ee3bdd4955616d6848492ff
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-06-20 04:41:44 +0300

    feat add TasksViewModel and DateTimeHandler in di modules

--- app/src/main/java/com/amsterdam/cutetudee/di/UiModule.kt
+++ app/src/main/java/com/amsterdam/cutetudee/di/UiModule.kt
@@ -1,9 +1,12 @@
 package com.amsterdam.cutetudee.di
 
 import com.amsterdam.cutetudee.presentation.screens.onBoarding.OnBoardingViewModel
+import com.amsterdam.cutetudee.presentation.screens.tasks.TasksViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
-val uiModule = module {
-    viewModelOf(::OnBoardingViewModel)
-}
+val uiModule =
+    module {
+        viewModelOf(::OnBoardingViewModel)
+        viewModelOf(::TasksViewModel)
+    }
