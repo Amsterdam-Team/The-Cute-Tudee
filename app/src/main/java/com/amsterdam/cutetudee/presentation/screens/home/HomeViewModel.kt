2cf33435ce6133df2b3ce50d9208be51b6178eb8
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-06-20 11:19:08 +0300

    refactor HomeViewModel to integrate theme management and state observation

--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/home/HomeViewModel.kt
@@ -1,13 +1,17 @@
 package com.amsterdam.cutetudee.presentation.screens.home
 
 import androidx.lifecycle.viewModelScope
+import com.amsterdam.cutetudee.R
 import com.amsterdam.cutetudee.domain.service.CategoryService
 import com.amsterdam.cutetudee.domain.service.TaskService
 import com.amsterdam.cutetudee.presentation.base.BaseViewModel
 import com.amsterdam.cutetudee.presentation.utils.IDateTimeHandler
+import com.amsterdam.cutetudee.presentation.utils.ThemeManager
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
@@ -18,7 +22,8 @@ import kotlinx.datetime.toLocalDateTime
 class HomeViewModel(
     private val taskService: TaskService,
     private val categoryService: CategoryService,
-    private val dateTimeHandler: IDateTimeHandler
+    private val dateTimeHandler: IDateTimeHandler,
+    private val themeManager: ThemeManager
 ) : BaseViewModel<Unit>(Unit) {
 
     private val _homeState = MutableStateFlow(HomeUiState())
@@ -28,7 +33,8 @@ class HomeViewModel(
         tryToExecute(
             function = {
                 _homeState.update { it.copy(isLoading = true) }
-                getCurrentDayTasks()
+                observeHomeStateChanges()
+                getCurrentTheme()
             },
             onSuccess = {},
             onError = { errorMessageId ->
@@ -36,17 +42,52 @@ class HomeViewModel(
             })
     }
 
-    private fun getCurrentDayTasks() {
+
+    fun onToggledAction() {
+        val isDarkMode = !homeState.value.isDarkMode
+        viewModelScope.launch {
+            try {
+                themeManager.updateTheme(
+                    isDarkMode
+                )
+                _homeState.update { it.copy(isDarkMode = isDarkMode) }
+            } catch (e: Exception) {
+                _homeState.update { it.copy(errorMessageId = R.string.error_unknown) }
+            }
+        }
+    }
+
+    private fun getCurrentTheme() {
+        viewModelScope.launch {
+            themeManager.initialize()
+            themeManager.themeFlow.filterNotNull().distinctUntilChanged().collect { theme ->
+                _homeState.update { it.copy(isDarkMode = theme) }
+            }
+        }
+    }
+
+    private fun observeHomeStateChanges() {
         val currentDate: LocalDate = Clock.System.now()
             .toLocalDateTime(TimeZone.currentSystemDefault())
             .date
         val tasksFlow = taskService.getTasksByDate(currentDate)
         val categoriesFlow = categoryService.getAllCategories()
+
         viewModelScope.launch {
             combine(tasksFlow, categoriesFlow) { tasks, categories ->
-                (tasks to categories).toHomeUiState(dateTimeHandler)
-            }.collect { combinedData ->
-                _homeState.update { combinedData }
+                val currentState = (tasks to categories).toHomeUiState(dateTimeHandler)
+
+                val moodState = when {
+                    currentState.doneTasksNumber == 0 -> MoodState.ZERO_PROGRESS
+                    currentState.totalTasksNumber == 0 -> MoodState.NOTHING_IN_YOUR_LIST
+                    currentState.totalTasksNumber == currentState.doneTasksNumber -> MoodState.TADOO
+                    else -> MoodState.STAY_WORKING
+                }
+
+                currentState.copy(moodState = moodState)
+
+            }.collect { finalState ->
+                _homeState.update { finalState }
             }
         }
     }
