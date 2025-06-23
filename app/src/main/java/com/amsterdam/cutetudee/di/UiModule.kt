d7cc5dc88d8716939ab27dc0e556265a609bd8be
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-06-23 11:45:12 +0300

    refactor: replace AppSettingsService with AppPreferencesDataStore in dependency injection

--- app/src/main/java/com/amsterdam/cutetudee/di/UiModule.kt
+++ app/src/main/java/com/amsterdam/cutetudee/di/UiModule.kt
@@ -1,18 +1,16 @@
 package com.amsterdam.cutetudee.di
 
-import com.amsterdam.cutetudee.MainViewModel
-import com.amsterdam.cutetudee.data.repository.AppSettingsServiceImpl
 import com.amsterdam.cutetudee.presentation.screens.category.CategoryViewModel
 import com.amsterdam.cutetudee.presentation.screens.categoryDetails.CategoryDetailsViewModel
 import com.amsterdam.cutetudee.presentation.screens.home.HomeViewModel
 import com.amsterdam.cutetudee.presentation.screens.onBoarding.OnBoardingViewModel
-import com.amsterdam.cutetudee.presentation.screens.splash.SplashViewModel
 import com.amsterdam.cutetudee.presentation.screens.tasks.AddEditTaskViewModel
 import com.amsterdam.cutetudee.presentation.screens.tasks.TasksViewModel
 import com.amsterdam.cutetudee.presentation.utils.DateTimeHandler
 import com.amsterdam.cutetudee.presentation.utils.IDateTimeHandler
 import com.amsterdam.cutetudee.presentation.utils.UriToBitmapString
 import com.amsterdam.cutetudee.presentation.utils.ValidateImageSize
+import com.amsterdam.cutetudee.presentation.screens.splash.SplashViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
@@ -25,9 +23,7 @@ val uiModule = module {
     single { ValidateImageSize(androidContext()) }
     viewModelOf(::CategoryViewModel)
     single<IDateTimeHandler> { DateTimeHandler() }
-    single { AppSettingsServiceImpl(get()) }
     viewModelOf(::HomeViewModel)
-    viewModelOf(::MainViewModel)
     viewModelOf(::CategoryDetailsViewModel)
     viewModelOf(::AddEditTaskViewModel)
 }
