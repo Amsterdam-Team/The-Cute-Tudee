7dab330b4dfc971439ace8a2c3e804d9a899213b
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-06-23 12:06:08 +0300

    refactor: simplify CuteTudeeApp by removing unused parameters and improving snackbar handling

--- app/src/main/java/com/amsterdam/cutetudee/presentation/CuteTudeeApp.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/CuteTudeeApp.kt
@@ -1,7 +1,6 @@
 package com.amsterdam.cutetudee.presentation
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
@@ -10,7 +9,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -20,75 +18,67 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.amsterdam.cutetudee.MainViewModel
 import com.amsterdam.cutetudee.presentation.component.custom_snack_bar.CustomSnackBar
 import com.amsterdam.cutetudee.presentation.component.custom_snack_bar.CustomSnackBarVisuals
 import com.amsterdam.cutetudee.presentation.navigation.CuteTudeeBottomNavigation
 import com.amsterdam.cutetudee.presentation.navigation.NavGraph
 import com.amsterdam.cutetudee.presentation.navigation.Screen
 import com.amsterdam.cutetudee.presentation.theme.AppTheme
-import com.amsterdam.cutetudee.presentation.theme.CuteTudeeTheme
 import kotlinx.coroutines.launch
-import org.koin.androidx.compose.koinViewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun CuteTudeeApp(
-    viewModel: MainViewModel = koinViewModel()
-) {
-    val isSystemInDarkTheme = viewModel.theme.collectAsState()
-    CuteTudeeTheme(isDarkTheme = isSystemInDarkTheme.value ?: isSystemInDarkTheme()) {
-        val snackBarHostState = remember { SnackbarHostState() }
-        val scope = rememberCoroutineScope()
-        val navController = rememberNavController()
-        val backStackEntry by navController.currentBackStackEntryAsState()
-        val currentDestination: NavDestination? = backStackEntry?.destination
+fun CuteTudeeApp() {
+    val snackBarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    val navController = rememberNavController()
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination: NavDestination? = backStackEntry?.destination
 
-        Box(Modifier.fillMaxSize()) {
-            Scaffold(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .navigationBarsPadding(),
-                containerColor = AppTheme.color.surface,
-                bottomBar = {
-                    CuteTudeeBottomNavigation(
-                        currentDestination = currentDestination,
-                        onNavigate = { destination ->
-                            navController.navigate(destination.screen) {
-                                launchSingleTop = true
-                                popUpTo(Screen.Home) {
-                                    inclusive = false
-                                }
+    Box(Modifier.fillMaxSize()) {
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize()
+                .navigationBarsPadding(),
+            containerColor = AppTheme.color.surface,
+            bottomBar = {
+                CuteTudeeBottomNavigation(
+                    currentDestination = currentDestination,
+                    onNavigate = { destination ->
+                        navController.navigate(destination.screen) {
+                            launchSingleTop = true
+                            popUpTo(Screen.Home) {
+                                inclusive = false
                             }
                         }
+                    }
+                )
+            }
+        ) { paddingValues ->
+            NavGraph(onShowSnackBar = { message, status ->
+                scope.launch {
+                    snackBarHostState.showSnackbar(
+                        CustomSnackBarVisuals(
+                            message = message,
+                            status = status,
+                        )
                     )
                 }
-            ) { paddingValues ->
-                NavGraph(navController = navController, onShowSnackBar = { message, status ->
-                    scope.launch {
-                        snackBarHostState.showSnackbar(
-                            CustomSnackBarVisuals(
-                                message = message,
-                                status = status,
-                            )
-                        )
-                    }
-                })
-            }
-
-            SnackbarHost(
-                hostState = snackBarHostState,
-                snackbar = { snackBarData ->
-                    val customVisuals = snackBarData.visuals as? CustomSnackBarVisuals
-                    if (customVisuals != null) {
-                        CustomSnackBar(customVisuals.message, customVisuals.status)
-                    }
-                },
-                modifier = Modifier
-                    .align(Alignment.TopCenter)
-                    .padding(top = 16.dp)
-            )
+            })
         }
+
+        SnackbarHost(
+            hostState = snackBarHostState,
+            snackbar = { snackBarData ->
+                val customVisuals = snackBarData.visuals as? CustomSnackBarVisuals
+                if (customVisuals != null) {
+                    CustomSnackBar(customVisuals.message, customVisuals.status)
+                }
+            },
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 16.dp)
+        )
     }
 }
 
