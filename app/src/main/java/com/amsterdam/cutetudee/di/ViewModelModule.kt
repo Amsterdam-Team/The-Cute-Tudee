505fd3c3895ecc06c63966f818770c94f2f39c89
Sara Ashraf <saraashraf1822004@gmail.com>
2025-06-23 15:40:18 +0300

    refactor: separate Koin modules for services, utils, and viewmodels

---
+++ app/src/main/java/com/amsterdam/cutetudee/di/ViewModelModule.kt
@@ -0,0 +1,23 @@
+package com.amsterdam.cutetudee.di
+
+import com.amsterdam.cutetudee.MainViewModel
+import com.amsterdam.cutetudee.presentation.screens.category.CategoryViewModel
+import com.amsterdam.cutetudee.presentation.screens.categoryDetails.CategoryDetailsViewModel
+import com.amsterdam.cutetudee.presentation.screens.home.HomeViewModel
+import com.amsterdam.cutetudee.presentation.screens.onBoarding.OnBoardingViewModel
+import com.amsterdam.cutetudee.presentation.screens.splash.SplashViewModel
+import com.amsterdam.cutetudee.presentation.screens.tasks.AddEditTaskViewModel
+import com.amsterdam.cutetudee.presentation.screens.tasks.TasksViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModelOf(::OnBoardingViewModel)
+    viewModelOf(::SplashViewModel)
+    viewModelOf(::TasksViewModel)
+    viewModelOf(::CategoryViewModel)
+    viewModelOf(::HomeViewModel)
+    viewModelOf(::MainViewModel)
+    viewModelOf(::CategoryDetailsViewModel)
+    viewModelOf(::AddEditTaskViewModel)
+}
