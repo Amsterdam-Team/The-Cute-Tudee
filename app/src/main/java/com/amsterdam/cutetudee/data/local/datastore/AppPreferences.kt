9628f78508c746e086f5f44d2394490ec0b1c821
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-06-24 21:41:16 +0300

    refactor: streamline onboarding and dark mode preference functions in AppPreferences

--- app/src/main/java/com/amsterdam/cutetudee/data/local/datastore/AppPreferences.kt
+++ app/src/main/java/com/amsterdam/cutetudee/data/local/datastore/AppPreferences.kt
@@ -17,28 +17,24 @@ class AppPreferences(
         const val DARK_MODE = "darkMode"
     }
 
-    // Function to get onboarding finished status
     suspend fun getIsOnBoardingFinished(): Boolean {
         return dataStore.data.map { settings ->
             settings[booleanPreferencesKey(ON_BOARDING)] ?: false
         }.first()
     }
 
-    // Function to set onboarding finished status
     suspend fun setIsOnBoardingFinished(isOnBoardingFinished: Boolean) {
         dataStore.edit { settings ->
             settings[booleanPreferencesKey(ON_BOARDING)] = isOnBoardingFinished
         }
     }
 
-    // Function to get dark mode status
     fun getAppDarkModeOn(): Flow<Boolean> {
         return dataStore.data.map { settings ->
             settings[booleanPreferencesKey(DARK_MODE)] ?: false
         }
     }
 
-    // Function to set dark mode status
     suspend fun setAppDarkModeOn(isDarkModeOn: Boolean) {
         dataStore.edit { settings ->
             settings[booleanPreferencesKey(DARK_MODE)] = isDarkModeOn
