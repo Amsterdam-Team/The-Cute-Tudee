637c39ac79b2800f715d53773b98bf5a9f8217a3
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-06-24 19:26:40 +0300

    refactor: update data modules to use PreferenceDataStore and rename AppSettingsService implementation

--- app/src/main/java/com/amsterdam/cutetudee/di/UtilsModule.kt
+++ app/src/main/java/com/amsterdam/cutetudee/di/UtilsModule.kt
@@ -2,7 +2,6 @@ package com.amsterdam.cutetudee.di
 
 import com.amsterdam.cutetudee.presentation.utils.DateTimeHandler
 import com.amsterdam.cutetudee.presentation.utils.IDateTimeHandler
-import com.amsterdam.cutetudee.presentation.utils.ThemeManager
 import com.amsterdam.cutetudee.presentation.utils.UriToBitmapString
 import com.amsterdam.cutetudee.presentation.utils.ValidateImageSize
 import org.koin.android.ext.koin.androidContext
@@ -10,7 +9,6 @@ import org.koin.dsl.module
 
 val utilsModule = module {
     single<IDateTimeHandler> { DateTimeHandler() }
-    single { ThemeManager(get()) }
     single { UriToBitmapString(androidContext()) }
     single { ValidateImageSize(androidContext()) }
 }
