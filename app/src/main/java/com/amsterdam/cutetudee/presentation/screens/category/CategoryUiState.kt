21241212a3d5466ae5e9994ea6321f31078bf470
Ahmed Al-Mancy <ahmedalmancy20@gmail.com>
2025-06-25 01:38:43 +0300

    Den haag update/refactor category mainscreen (#127)
    * removed showError data object
    
    * add both interfaces for Category and CategoryAdd interaction listeners
    
    * refactor to implement the new Interaction Listener
    
    * remove error messages from the data classes
    
    * refactor to use the default view model and implement the new interaction listeners
    
    * add `onCancel` to `CategoryAddBottomSheet`
    
    ---------
    
    Co-authored-by: Nabil Bouzine <nabilsocialnetwork@gmail.com>

--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/category/CategoryUiState.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/category/CategoryUiState.kt
@@ -1,16 +1,12 @@
 package com.amsterdam.cutetudee.presentation.screens.category
 
-
 import android.net.Uri
-import androidx.annotation.StringRes
-import androidx.compose.ui.graphics.painter.Painter
 
 data class CategoryScreenUiState(
     val categories: List<CategoryUiState> = emptyList(),
     val isLoading: Boolean = false,
     val addBottomSheet: BottomSheetState = BottomSheetState(),
     val hideBottomSheet: Boolean = true,
-    val errorMessageResourceId: Int? = null
 )
 
 data class BottomSheetState(
@@ -18,7 +14,6 @@ data class BottomSheetState(
     val image: Uri = Uri.EMPTY,
     val isEnabled: Boolean = false,
     val isLoading: Boolean = false,
-    @StringRes val error: Int? = null
 )
 
 data class CategoryUiState(
