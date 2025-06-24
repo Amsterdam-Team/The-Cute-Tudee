0548acde6da87de0f1c77d37f7373b444149c5ef
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-06-24 20:10:39 +0300

    refactor: update HomeViewModel to integrate AppSettingsService for dark mode handling

--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/home/HomeViewModel.kt
@@ -2,6 +2,7 @@ package com.amsterdam.cutetudee.presentation.screens.home
 
 import androidx.lifecycle.viewModelScope
 import com.amsterdam.cutetudee.R
+import com.amsterdam.cutetudee.domain.service.AppSettingsService
 import com.amsterdam.cutetudee.domain.service.CategoryService
 import com.amsterdam.cutetudee.domain.service.TaskService
 import com.amsterdam.cutetudee.presentation.base.BaseViewModel
@@ -10,7 +11,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
@@ -22,21 +22,28 @@ class HomeViewModel(
     private val taskService: TaskService,
     private val categoryService: CategoryService,
     private val dateTimeHandler: IDateTimeHandler,
+    private val appSettingsService: AppSettingsService
 ) : BaseViewModel<Unit>(Unit) {
 
     private val _homeState = MutableStateFlow(HomeUiState())
     val homeState = _homeState.asStateFlow()
 
     init {
-        tryToExecute(
-            function = {
-                _homeState.update { it.copy(isLoading = true) }
-                observeHomeStateChanges()
-            },
-            onSuccess = {},
-            onError = { errorMessageId ->
-                _homeState.update { it.copy(errorMessageId = errorMessageId, isLoading = false) }
-            })
+        viewModelScope.launch {
+            appSettingsService.isDarkMode().distinctUntilChanged().collect { isDarkMode ->
+                _homeState.update { it.copy(isDarkMode = isDarkMode) }
+            }
+        }
+        tryToExecute(function = {
+            _homeState.update {
+                it.copy(
+                    isLoading = true
+                )
+            }
+            observeHomeStateChanges()
+        }, onSuccess = {}, onError = { errorMessageId ->
+            _homeState.update { it.copy(errorMessageId = errorMessageId, isLoading = false) }
+        })
     }
 
     fun onFabAction() {
@@ -51,6 +58,7 @@ class HomeViewModel(
         val isDarkMode = !homeState.value.isDarkMode
         viewModelScope.launch {
             try {
+                appSettingsService.setDarkMode(isDarkMode)
                 _homeState.update { it.copy(isDarkMode = isDarkMode) }
             } catch (e: Exception) {
                 _homeState.update { it.copy(errorMessageId = R.string.error_unknown) }
@@ -59,9 +67,8 @@ class HomeViewModel(
     }
 
     private fun observeHomeStateChanges() {
-        val currentDate: LocalDate = Clock.System.now()
-            .toLocalDateTime(TimeZone.currentSystemDefault())
-            .date
+        val currentDate: LocalDate =
+            Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
         val tasksFlow = taskService.getTasksByDate(currentDate)
         val categoriesFlow = categoryService.getAllCategories()
 
