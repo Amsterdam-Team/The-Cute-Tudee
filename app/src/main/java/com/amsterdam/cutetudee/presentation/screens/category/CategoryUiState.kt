efda1b6630f80a908e7220fdab30875bcc7a26ed
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-06-24 14:56:47 +0300

    update both ui data class states to be able to use the category bottom sheet ui state

--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/category/CategoryUiState.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/category/CategoryUiState.kt
@@ -1,26 +1,14 @@
 package com.amsterdam.cutetudee.presentation.screens.category
 
-
-import android.graphics.Bitmap
-import android.net.Uri
-import androidx.annotation.StringRes
 import androidx.compose.ui.graphics.painter.Painter
+import com.amsterdam.cutetudee.presentation.component.AddCategoryBottomSheet.CategoryBottomSheetUIState
 
 data class CategoryScreenUiState(
     val categories: List<CategoryUiState> = emptyList(),
     val isLoading: Boolean = false,
-    val addBottomSheet: BottomSheetState = BottomSheetState(),
+    val addBottomSheet: CategoryBottomSheetUIState = CategoryBottomSheetUIState(),
     val hideBottomSheet: Boolean = true,
     val errorMessageResourceId: Int? = null
-)
-
-data class BottomSheetState(
-    val name: String = "",
-    val image: Uri = Uri.EMPTY,
-    val painter: Painter? = null,
-    val isEnabled: Boolean = false,
-    val isLoading: Boolean = false,
-    @StringRes val error: Int? = null
 )
 
 data class CategoryUiState(
--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/categoryDetails/CategoryDetailsUiState.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/categoryDetails/CategoryDetailsUiState.kt
@@ -2,13 +2,13 @@ package com.amsterdam.cutetudee.presentation.screens.categoryDetails
 
 import com.amsterdam.cutetudee.domain.model.Category
 import com.amsterdam.cutetudee.domain.model.Task
-import com.amsterdam.cutetudee.presentation.screens.category.BottomSheetState
+import com.amsterdam.cutetudee.presentation.component.AddCategoryBottomSheet.CategoryBottomSheetUIState
 import kotlin.uuid.ExperimentalUuidApi
 
 data class CategoryDetailsUiState(
     val isLoading: Boolean = true,
     val errorMessage: String = "",
-    val addBottomSheet: BottomSheetState = BottomSheetState(),
+    val addBottomSheet: CategoryBottomSheetUIState = CategoryBottomSheetUIState(),
     val hideBottomSheet: Boolean = true,
     val taskUiState: List<TaskUiState> = emptyList(),
     val categoryUiState: CategoryUiState = CategoryUiState()
