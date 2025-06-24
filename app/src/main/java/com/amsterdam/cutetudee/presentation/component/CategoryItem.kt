2ec6ec68ec56b37ca9874dbd0cabea8b3b8d48ab
Eslam <eslam.mohamed.551999@gmail.com>
2025-06-24 16:30:06 +0300

    fix image size of category item add max lines to category label

--- app/src/main/java/com/amsterdam/cutetudee/presentation/component/CategoryItem.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/component/CategoryItem.kt
@@ -3,6 +3,7 @@ package com.amsterdam.cutetudee.presentation.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,7 +15,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -25,30 +28,36 @@ import com.amsterdam.cutetudee.presentation.utils.ThemeAndLocalePreviews
 
 @Composable
 fun CategoryItem(
-    modifier: Modifier = Modifier,
     categoryName: String,
     categoryImage: Painter,
+    modifier: Modifier = Modifier,
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = modifier
     ) {
-        Image(
+        Box(
             modifier = Modifier
                 .padding(bottom = 8.dp)
                 .fillMaxWidth()
                 .aspectRatio(1f)
                 .clip(CircleShape)
-                .background(AppTheme.color.surfaceHigh)
-                .padding(23.dp),
-            painter = categoryImage,
-            contentDescription = null,
-        )
+                .clipToBounds()
+                .background(AppTheme.color.surfaceHigh),
+            contentAlignment = Alignment.Center,
+        ) {
+            Image(
+                painter = categoryImage,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+            )
+        }
         Text(
             text = categoryName,
             style = AppTheme.textStyle.label.small,
-            color = AppTheme.color.body
+            color = AppTheme.color.body,
+            maxLines = 1
         )
     }
 }
