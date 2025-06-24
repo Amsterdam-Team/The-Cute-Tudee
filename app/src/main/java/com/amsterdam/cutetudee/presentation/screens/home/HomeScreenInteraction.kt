a0e3d4cfe702ead3a0447614b42b1b770346dc77
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-06-25 01:18:07 +0300

    feat add HomeScreenInteraction interface

---
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/home/HomeScreenInteraction.kt
@@ -0,0 +1,19 @@
+package com.amsterdam.cutetudee.presentation.screens.home
+
+import com.amsterdam.cutetudee.presentation.model.TaskUi
+
+interface HomeScreenInteraction {
+    fun onAddTaskClicked()
+
+    fun onDismissAddBottomSheet()
+
+    fun onTaskClicked(task: TaskUi)
+
+    fun onDismissTaskDetailsBottomSheet()
+
+    fun onEditTaskClicked()
+
+    fun onDismissEditBottomSheet()
+
+    fun onChangeMode()
+}
