4d6aa812cff8629265cb46710bc5d0bd626aa6c1
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-06-24 20:08:05 +0300

    refactor: update MainViewModel to use Boolean for theme state and improve dark mode handling

--- app/src/main/java/com/amsterdam/cutetudee/MainViewModel.kt
+++ app/src/main/java/com/amsterdam/cutetudee/MainViewModel.kt
@@ -2,7 +2,6 @@ package com.amsterdam.cutetudee
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.amsterdam.cutetudee.domain.model.ThemeMode
 import com.amsterdam.cutetudee.domain.service.AppSettingsService
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -11,12 +10,12 @@ import kotlinx.coroutines.launch
 class MainViewModel(
     private val appSettingsService: AppSettingsService
 ) : ViewModel() {
-    val _themeState: MutableStateFlow<ThemeMode> = MutableStateFlow(ThemeMode.LIGHT)
+    val _themeState: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val themeState = _themeState.asStateFlow()
 
     init {
         viewModelScope.launch {
-            appSettingsService.getPreferredMode().collect { mode ->
+            appSettingsService.isDarkMode().collect { mode ->
                 _themeState.value = mode
             }
         }
