f8fda02ad0bd568d4d320a39f3936f5e197c1f18
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-06-24 05:58:31 +0300

    refactor enhance HomeScreen paddings

--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/home/HomeScreen.kt
@@ -2,12 +2,11 @@ package com.amsterdam.cutetudee.presentation.screens.home
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
@@ -103,11 +102,12 @@ fun HomeScreenContent(
                 onSwitchTheme = onSwitchTheme,
             )
             LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(24.dp),
+                contentPadding = PaddingValues(bottom = 82.dp),
                 modifier =
                     Modifier
                         .weight(1f)
                         .background(AppTheme.color.surface),
-                contentPadding = PaddingValues(bottom = 82.dp),
             ) {
                 item {
                     OverlayBoxContent(
@@ -124,20 +124,21 @@ fun HomeScreenContent(
                         TaskSection(
                             title = stringResource(R.string.in_progress),
                             tasks = homeUiState.inProgressTasks,
+                            modifier = Modifier,
                         )
                     }
                     item {
-                        Spacer(modifier = Modifier.height(24.dp))
                         TaskSection(
                             title = stringResource(R.string.todo),
                             tasks = homeUiState.todoTasks,
+                            modifier = Modifier,
                         )
                     }
                     item {
-                        Spacer(modifier = Modifier.height(24.dp))
                         TaskSection(
                             title = stringResource(R.string.done),
                             tasks = homeUiState.doneTasks,
+                            modifier = Modifier,
                         )
                     }
                 } else {
