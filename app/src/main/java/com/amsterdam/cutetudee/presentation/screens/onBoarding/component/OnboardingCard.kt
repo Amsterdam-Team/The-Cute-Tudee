ea9538fc04b1e6cd4614aa3c15b9e422a8fbc368
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-06-25 18:21:23 +0300

    refactor: enhance OnboardingCard layout with RTL support and improve readability

--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/onBoarding/component/OnboardingCard.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/onBoarding/component/OnboardingCard.kt
@@ -12,9 +12,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.amsterdam.cutetudee.R
 import com.amsterdam.cutetudee.presentation.component.CustomFloatingActionButton
@@ -25,53 +28,57 @@ import com.amsterdam.cutetudee.presentation.utils.ThemeAndLocalePreviews
 
 @Composable
 fun OnboardingCard(
-    modifier: Modifier = Modifier,
-    title: String, description: String,
-    onButtonClick: () -> Unit
+    modifier: Modifier = Modifier, title: String, description: String, onButtonClick: () -> Unit
 ) {
-        Box(modifier
-            .padding(bottom = 32.dp)) {
-            Column(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .background(AppTheme.color.onPrimaryCard, RoundedCornerShape(32.dp))
-                    .border(
-                        width = 1.dp,
-                        color = AppTheme.color.onPrimaryStroke,
-                        shape = RoundedCornerShape(32.dp)
-                    )
-                    .padding(start = 16.dp, end = 16.dp, top = 24.dp, bottom = 43.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = title,
-                    textAlign = TextAlign.Center,
-                    style = AppTheme.textStyle.title.medium,
-                    minLines = 2,
-                    color = AppTheme.color.title
+    Box(
+        modifier.padding(bottom = 32.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .background(AppTheme.color.onPrimaryCard, RoundedCornerShape(32.dp))
+                .border(
+                    width = 1.dp,
+                    color = AppTheme.color.onPrimaryStroke,
+                    shape = RoundedCornerShape(32.dp)
                 )
-                VerticalSpacer(16.dp)
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = description,
-                    textAlign = TextAlign.Center,
-                    style = AppTheme.textStyle.body.medium,
-                    color = AppTheme.color.body,
-                    minLines = 3
-                )
-            }
-            CustomFloatingActionButton(
-                modifier = Modifier
-                    .offset(y = 32.dp)
-                    .align(Alignment.BottomCenter),
-                iconDrawable = painterResource(id = R.drawable.arrow_right_double_icon),
-                onClick = onButtonClick, isLoading = false
+                .padding(start = 16.dp, end = 16.dp, top = 24.dp, bottom = 43.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = title,
+                textAlign = TextAlign.Center,
+                style = AppTheme.textStyle.title.medium,
+                minLines = 2,
+                color = AppTheme.color.title
             )
+            VerticalSpacer(16.dp)
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = description,
+                textAlign = TextAlign.Center,
+                style = AppTheme.textStyle.body.medium,
+                color = AppTheme.color.body,
+                minLines = 3
+            )
+        }
+        val layoutDirection = LocalLayoutDirection.current
+
+        val floatingActionButtonRotate = if (layoutDirection == LayoutDirection.Rtl) 180f else 0f
+        CustomFloatingActionButton(
+            modifier = Modifier
+                .offset(y = 32.dp)
+                .align(Alignment.BottomCenter)
+                .rotate(floatingActionButtonRotate),
+            iconDrawable = painterResource(id = R.drawable.arrow_right_double_icon),
+            onClick = onButtonClick,
+            isLoading = false
+        )
     }
 }
 
-@ThemeAndLocalePreviews ()
+@ThemeAndLocalePreviews()
 @Composable
 private fun OnboardingCardPreviewDark() {
     CuteTudeeTheme() {
