52728dee984caceac5a5824cff6dbb37a67d6433
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-06-26 22:36:57 +0300

    Update category details transitions

--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/categoryDetails/CategoryDetailsScreenRoute.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/categoryDetails/CategoryDetailsScreenRoute.kt
@@ -1,5 +1,10 @@
 package com.amsterdam.cutetudee.presentation.screens.categoryDetails
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -9,7 +14,14 @@ import com.amsterdam.cutetudee.presentation.navigation.Screen
 fun NavGraphBuilder.categoryDetailsScreenRoute(
     onShowSnackBar: (message: String, status: CustomSnackBarStatus) -> Unit
 ) {
-    composable<Screen.CategoryDetails> {
+    composable<Screen.CategoryDetails>(
+        enterTransition = {
+            slideInHorizontally { it / 3 } + fadeIn(animationSpec = tween(300, delayMillis = 90))
+        },
+        exitTransition = {
+            slideOutHorizontally { -it / 3 } + fadeOut(animationSpec = tween(90))
+        },
+    ) {
         CategoryDetailsScreen(onShowSnackBar = onShowSnackBar)
     }
 }
