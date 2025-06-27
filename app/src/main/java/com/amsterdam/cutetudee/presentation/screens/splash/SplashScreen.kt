772acbdf55f3bf6b82c6c91e21633b466b247463
noor serry <noor.serry.94@gmail.com>
2025-06-27 04:19:32 +0300

    fix onBoardingBackground image position

--- app/src/main/java/com/amsterdam/cutetudee/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/amsterdam/cutetudee/presentation/screens/splash/SplashScreen.kt
@@ -3,13 +3,12 @@ package com.amsterdam.cutetudee.presentation.screens.splash
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -55,9 +54,7 @@ private fun SplashContent() {
             painter = painterResource(AppTheme.images.onBoardingBackground),
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.7f)
+            modifier = Modifier.align(AbsoluteAlignment.TopRight)
         )
         Box(
             modifier = Modifier.align(Alignment.Center)
